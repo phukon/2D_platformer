d9e79dc6ffe176bd61b77f6e09865aa7257a8683
Riki Phukon <rikiphukon17@gmail.com>
2021-06-27 22:14:55 +0530

    Animation
    Added coin catch animation. Coin bounces before disappearing.

--- coin.gd
+++ coin.gd
@@ -5,4 +5,8 @@ extends Area2D
 
 
 func _on_coin_body_entered(body: Node) -> void:
+	$AnimationPlayer.play("bounce_animation")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	queue_free()
