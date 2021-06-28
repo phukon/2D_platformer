8009716d30cdecd65d6d5fb717af42c3bb6bc961
Riki Phukon <rikiphukon17@gmail.com>
2021-06-28 07:29:40 +0530

    Added coin counter

--- Actors/Kon.gd
+++ Actors/Kon.gd
@@ -2,11 +2,12 @@ extends KinematicBody2D
 
 
 var velocity = Vector2(0, 0)
+var coins = 0
 const SPEED = 350
 const GRAVITY = 35
 const JUMPFORCE = -1000
 
-	
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
@@ -18,18 +19,23 @@ func _physics_process(delta: float) -> void:
 		$Sprite.flip_h = true
 	else:
 		$Sprite.play("idle")
-	
+
 	if not is_on_floor():
 		$Sprite.play("air")
 	velocity.y += GRAVITY
-	
+
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMPFORCE
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
 	velocity.x = lerp(velocity.x, 0, 0.2)
+
+	if coins == 3:
+		get_tree().change_scene("res://Levels/Level1.tscn")
 
 
 func _on_fall_zone_body_entered(body: Node) -> void:
 	get_tree().change_scene("res://Levels/Level1.tscn")
+
+func add_coin():
+	coins += 1
--- coin.gd
+++ coin.gd
@@ -6,7 +6,8 @@ extends Area2D
 
 func _on_coin_body_entered(body: Node) -> void:
 	$AnimationPlayer.play("bounce_animation")
-
+	body.add_coin() #will not work if one of the coin objects have entered or
+# overlapped one of the platforms(or other static objects).
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	queue_free()
