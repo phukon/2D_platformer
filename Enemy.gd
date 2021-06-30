b59c3c1f0889710ddc0f27f5d348436e67889dc4
Riki Phukon <rikiphukon17@gmail.com>
2021-06-30 21:32:43 +0530

    Enemy collisions

--- Enemy.gd
+++ Enemy.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+var speed = 50
 var velocity = Vector2()
 export var direction =  -1
 export var detects_cliffs = true
@@ -18,5 +19,23 @@ func _physics_process(delta: float) -> void:
 		$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
 	
 	velocity.y += 20
-	velocity.x = 50 * direction
+	velocity.x = speed * direction
 	velocity = move_and_slide(velocity, Vector2.UP) #or else the gravity will keep adding
+
+
+func _on_top_checker_body_entered(body: Node) -> void:
+	$AnimatedSprite.play("squashed")
+	speed = 0
+	set_collision_layer_bit(4, false)
+	set_collision_mask_bit(0, false)
+	$top_checker.set_collision_layer_bit(4, false) #cuz top_checker has got it's on collision layer and masks
+	$top_checker.set_collision_mask_bit(0, false)
+	$side_checker.set_collision_layer_bit(4, false)
+	$side_checker.set_collision_mask_bit(0, false)#or else the player will hit the side checker after the top checker is removed from the mask and layers
+	$Timer.start()
+
+func _on_side_checker_body_entered(body: Node) -> void:
+	get_tree().change_scene("res://Levels/Level1.tscn")
+
+func _on_Timer_timeout() -> void:
+	queue_free()
